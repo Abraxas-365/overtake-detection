828b8a12550ae01172df7796e6cc05250de2b1cd
Abraxas-365 <luisfmiranda8@gmail.com>
2026-02-12 17:19:34 +0000

    archt

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -1,10 +1,3 @@
 // src/pipeline/mod.rs
 
-pub mod event_bus;
-pub mod frame_context;
 pub mod legality_buffer;
-pub mod metrics;
-
-pub use event_bus::{EventBus, PipelineEvent};
-pub use frame_context::FrameContext;
-pub use metrics::PipelineMetrics;
